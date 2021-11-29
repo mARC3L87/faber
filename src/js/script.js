fcf0a1ae38c7b8d8da8cb4db450002eefc239e42
Marcel Lukomski <marcel.lukomski@gmail.com>
2021-11-29 22:17:32 +0100

    Add media queries to small screen

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,40 @@
 const imageRow = document.querySelector('.image-row');
-const fotoBox = document.querySelectorAll('.foto-box');
+const fotoBox = document.querySelectorAll(
+  '.image-container .image-row .foto-box'
+);
 const btnRight = document.querySelector('.btn-right');
 const btnLeft = document.querySelector('.btn-left');
+const oneFotoBox = document.querySelector(
+  '.image-container .image-row .foto-box'
+);
+const style = getComputedStyle(oneFotoBox);
+const margin = parseInt(style.marginLeft) + parseInt(style.marginRight);
+const size = fotoBox[0].offsetWidth + margin;
 
-let counter = 1;
-const size = fotoBox[0].clientWidth;
+const initCounter = () => {
+  let counter = 0;
+  return () => {
+    btnRight.addEventListener('click', (e) => {
+      if (counter < fotoBox.length - 1) {
+        imageRow.style.transition = 'transform 0.4s ease-in-out';
+        counter++;
+        imageRow.style.transform = `translateX(${-size * counter}px)`;
+      } else {
+        imageRow.style.transition = 'transform 0.4s ease-in-out';
+        imageRow.style.transform = `translateX(0)`;
+        counter = 0;
+      }
+    });
 
-//imageRow.style.transform = `translateX(${-size * counter}px)`;
-
-btnRight.addEventListener('click', (e) => {
-  if (counter < 4) {
-    imageRow.style.transition = 'transform 0.4s ease-in-out';
-    counter++;
-    imageRow.style.transform = `translateX(${-size * counter}px)`;
-    console.log(counter);
-  } else {
-    imageRow.style.transition = 'transform 0.4s ease-in-out';
-    imageRow.style.transform = `translateX(0)`;
-    counter = 1;
-  }
-});
-
-btnLeft.addEventListener('click', (e) => {
-  if (counter <= 0) {
-    return;
-  }
-  imageRow.style.transition = 'transform 0.4s ease-in-out';
-  counter--;
-  imageRow.style.transform = `translateX(${-size * counter}px`;
-  console.log(counter);
-});
+    btnLeft.addEventListener('click', (e) => {
+      if (counter <= 0) {
+        return;
+      }
+      imageRow.style.transition = 'transform 0.4s ease-in-out';
+      counter--;
+      imageRow.style.transform = `translateX(${-size * counter}px`;
+    });
+  };
+};
+const counterGallery = initCounter();
+counterGallery();
